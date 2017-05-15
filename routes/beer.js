cc8064d09872001682cbc3c83297e368b2f290da
JiSooYoon <jisooyoon@yunjisuui-MacBook-Air.local>
2017-05-15 18:06:05 +0900

    add route beer

--- routes/beer.js
+++ routes/beer.js
@@ -7,22 +7,52 @@ var db = require('../db');
 
 /* GET beer detail. */
 router.get('/', function(req, res, next) {
-    res.send('GET beer');
+    db.query("SELECT * FROM beer WHERE beer_id = ?;", [req.body.beer_id])
+        .then(function (data) {
+            res.json({
+                resultCode: 0,
+                info: data
+            });
+        })
+        .catch(function (err) {
+            res.json({
+                resultCode: -1,
+                msg: 'This beer does not exist.'
+            });
+        });
 });
 
 /* GET beer list. */
 router.get('/', function(req, res, next) {
-    res.send('GET beer list');
-});
-
-/* SET beer. */
-router.get('/', function(req, res, next) {
-    res.send('SET beer');
+    db.query("SELECT * FROM beer;", [])
+        .then(function (data) {
+            res.json({
+                resultCode: 0,
+                info: data
+            });
+        })
+        .catch(function (err) {
+            res.json({
+                resultCode: -1,
+                msg: 'The beer-table does not exist.'
+            });
+        });
 });
 
 /* DELETE beer. */
 router.get('/', function(req, res, next) {
-    res.send('DEL beer');
+    db.query("DELETE FROM beer WHERE beer_id = ?;", [req.body.beer_id])
+        .then(function () {
+            res.json({
+                resultCode: 0
+            })
+        })
+        .catch(function (err) {
+            res.json({
+                resultCode: -1,
+                msg: err
+            })
+        });
 });
 
 module.exports = router;
