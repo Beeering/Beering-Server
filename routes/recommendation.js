caa447cda710c5d4e761ecfed7affc09a8e7d07f
JiSooYoon <jisooyoon@yunjisuui-MacBook-Air.local>
2017-06-14 00:08:24 +0900

    recommend connection test

--- routes/recommendation.js
+++ routes/recommendation.js
@@ -20,8 +20,7 @@ router.get('/get', function(req, res, next) {
                 body: JSON.stringify({
                     "event": "$set",
                     "entityType": "user",
-                    "entityId": "u0",
-                    "eventTime": "현재 시각"
+                    "entityId": "u0"
                 })
             }
         ]
@@ -40,48 +39,6 @@ router.get('/get', function(req, res, next) {
             })
         }
     });
-/*
-    -d '{
-    "event" : "$set",
-        "entityType" : "user",
-        "entityId" : "u0",
-        "eventTime" : "2014-11-02T09:39:45.618-08:00"
-}'
-  */
-/*
-    request({
-            method: 'PUT',
-            preambleCRLF: true,
-            postambleCRLF: true,
-            uri: 'http://service.com/upload',
-            multipart: [
-                {
-                    'content-type': 'application/json',
-                    body: JSON.stringify({foo: 'bar', _attachments: {'message.txt': {follows: true, length: 18, 'content_type': 'text/plain' }}})
-                },
-                { body: 'I am an attachment' },
-                { body: fs.createReadStream('image.png') }
-            ],
-            // alternatively pass an object containing additional options
-            multipart: {
-                chunked: false,
-                data: [
-                    {
-                        'content-type': 'application/json',
-                        body: JSON.stringify({foo: 'bar', _attachments: {'message.txt': {follows: true, length: 18, 'content_type': 'text/plain' }}})
-                    },
-                    { body: 'I am an attachment' }
-                ]
-            }
-        },
-        function (error, response, body) {
-            if (error) {
-                return console.error('upload failed:', error);
-            }
-            console.log('Upload successful!  Server responded with:', body);
-        })
-*/
-
 });
 
 module.exports = router;
